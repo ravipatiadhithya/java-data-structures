9ad0b31cbab3d4fc285d8a5240ced56b9c37f9c2
Adhithya Ravipati <ravipatiadhithya@gmail.com>
2018-07-15 10:57:02 -0700

    Adding IllegalStateException to add child node methods

--- src/com/adhithyaravipati/java/datastructures/tree/BinaryTree.java
+++ src/com/adhithyaravipati/java/datastructures/tree/BinaryTree.java
@@ -22,11 +22,11 @@ public interface BinaryTree<E> {
 	
 	Position<E> left(Position<E> position) throws IllegalArgumentException;
 	
-	void addLeft(Position<E> position) throws IllegalArgumentException;
+	void addLeft(Position<E> position) throws IllegalArgumentException, IllegalStateException;
 	
 	Position<E> right(Position<E> position) throws IllegalArgumentException;
 	
-	void addRight(Position<E> position) throws IllegalArgumentException;
+	void addRight(Position<E> position) throws IllegalArgumentException, IllegalStateException;
 	
 	Position<E> sibling(Position<E> position) throws IllegalArgumentException;
 
