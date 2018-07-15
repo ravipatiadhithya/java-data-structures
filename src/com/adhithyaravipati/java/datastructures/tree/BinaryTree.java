64629b73086316704b26c8f9bd1747fb2ead46b7
Adhithya Ravipati <ravipatiadhithya@gmail.com>
2018-07-15 10:50:02 -0700

    BinaryTree: Adding interface

---
+++ src/com/adhithyaravipati/java/datastructures/tree/BinaryTree.java
@@ -0,0 +1,33 @@
+package com.adhithyaravipati.java.datastructures.tree;
+
+import com.adhithyaravipati.java.datastructures.model.Position;
+
+public interface BinaryTree<E> {
+	
+	boolean isEmpty();
+	
+	int size();
+	
+	boolean isRoot(Position<E> position) throws IllegalArgumentException;
+	
+	boolean isInternal(Position<E> position) throws IllegalArgumentException;
+	
+	boolean isExternal(Position<E> position) throws IllegalArgumentException;
+	
+	int numOfChildren(Position<E> position) throws IllegalArgumentException;
+	
+	Position<E> root();
+	
+	Position<E> parent(Position<E> position) throws IllegalArgumentException;
+	
+	Position<E> left(Position<E> position) throws IllegalArgumentException;
+	
+	void addLeft(Position<E> position) throws IllegalArgumentException;
+	
+	Position<E> right(Position<E> position) throws IllegalArgumentException;
+	
+	void addRight(Position<E> position) throws IllegalArgumentException;
+	
+	Position<E> sibling(Position<E> position) throws IllegalArgumentException;
+
+}
